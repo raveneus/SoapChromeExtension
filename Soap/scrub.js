5e729d25c09f58bcdb722cf26a4619849b0f86f1
raveneus <raveneus.cwillguy@gmail.com>
2016-11-10 09:33:13 -0600

    Update scrub.js

--- Soap/scrub.js
+++ Soap/scrub.js
@@ -5,13 +5,13 @@ function scrub(doc_elem) {
   if (typeof doc_elem !== "string") {
     return;
   }
-  for (int i = 0; i < bad_words.length; i++) {
+  for (var i = 0; i < bad_words.length; i++) {
     if (eval(doc_elem + ".toLowerCase().indexOf(bad_words[i])") !== -1) {
       eval(doc_elem + ".replace(bad_words[i], Array(bad_words[i].length + 1).join('*'))");
     }
   }
   
-  for (int i = 0; i < bad_words.length; i++) {
+  for (var i = 0; i < bad_words.length; i++) {
     if (eval(doc_elem + ".toLowerCase().indexOf(insults[i])") !== -1) {
       eval(doc_elem + ".replace(insults[i], Array(insults[i].length + 1).join('*'))");
     }
