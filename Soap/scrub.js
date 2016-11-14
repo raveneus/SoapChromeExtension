df1ef20109879ef6a1e8421daf96f75d5e3e08c2
raveneus <raveneus.cwillguy@gmail.com>
2016-11-14 08:52:03 -0600

    Update scrub.js

--- Soap/scrub.js
+++ Soap/scrub.js
@@ -3,31 +3,31 @@ var insults = ["screw you"]; // leave blank until later
 
 function scrub(doc_elem) {
   
-  for (var i = 0; i < bad_words.length; i++) {
-    if (doc_elem.innerHTML.indexOf(bad_words[i]) !== -1) {
-	  console.log("bad word found");
-	  var string = Array(bad_words[i].length + 1).join('*');
-	  var re = new RegExp(bad_words[i], 'gi');
-      doc_elem = doc_elem.replace(re, string);
+    for (var i = 0; i < bad_words.length; i++) {
+        if (doc_elem.innerHTML.indexOf(bad_words[i]) !== -1) {
+	        console.log("bad word found");
+	        var string = Array(bad_words[i].length + 1).join('*');
+	        var re = new RegExp(bad_words[i], 'gi');
+            doc_elem = doc_elem.replace(re, string);
+        }
     }
-  }
   
-  for (var i = 0; i < insults.length; i++) {
-    if (doc_elem.innerHTML.indexOf(insults[i]) !== -1) {
-	  console.log("insult found");
-	  var string = Array(insults[i].length + 1).join('*');
-	  var re = new RegExp(insults[i], 'gi');
-      doc_elem = doc_elem.replace(re, string);
+    for (var i = 0; i < insults.length; i++) {
+        if (doc_elem.innerHTML.indexOf(insults[i]) !== -1) {
+	        console.log("insult found");
+	        var string = Array(insults[i].length + 1).join('*');
+	        var re = new RegExp(insults[i], 'gi');
+            doc_elem = doc_elem.replace(re, string);
+        }
     }
-  }
 }
 
 function scrub_interact() {
-  scrub("document.title");
-  var elements = document.querySelectorAll("*");
-  for (var i = 0; i < elements.length; i++) {
-    scrub(elements[i]);
-  }
+    document.title = scrub(document.title);
+    var elements = document.querySelectorAll("*");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i] = scrub(elements[i]);
+    }
 }
 
 setTimeout(scrub_interact, 1);
